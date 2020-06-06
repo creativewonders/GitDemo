b82c6369fda7261f21813ad2f6eaf8eb80e4cda0
Vishali <vishali.kamenani@gmail.com>
2020-06-06 07:58:08 -0400

    American time commit

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -9,6 +9,10 @@ public class RestAPITest {
 		System.out.println("postJira0");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
+		
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
 	}
 	
 	@Test
