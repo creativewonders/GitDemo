f651277219f56da33ef88ce9e455ba18767883dd
Vishali <vishali.kamenani@gmail.com>
2020-06-08 07:22:46 -0400

    Architect US Development

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -7,11 +7,31 @@ public class SeleniumTest {
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAuto");
+		System.out.println("WebAuto");
 	}
 	
 	@Test
-	public void elementsUi()
+	public void elementsUiparts()
 	{
-		System.out.println("elementsUi");
+		System.out.println("elementsUi0");
+		System.out.println("elementsUi1");
+		System.out.println("elementsUi2");
+		System.out.println("elementsUi3");
+		
+		System.out.println("elementsUi4");
+		System.out.println("elementsUi5");
+	}
+	
+	
+	@Test
+	public void Tools()
+	{
+		System.out.println("tools0");
+		System.out.println("tools1");
+		System.out.println("tools2");
+		System.out.println("tools03");
+		
+		System.out.println("tools04");
+		System.out.println("tools05");
 	}
 }
